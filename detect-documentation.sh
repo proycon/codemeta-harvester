9583852103022a8d1129b348295680ba22b0553d
Maarten van Gompel <proycon@anaproy.nl>
2022-04-12 11:12:22 +0200

    fix for documentation URL detector

--- detect-documentation.sh
+++ detect-documentation.sh
@@ -2,5 +2,5 @@
 
 # This script attempts to detect some common documentation URLs from standard input
 # it should passed the README.md file and will return URLs
-grep -o -E '(https?://[a-z0-9_A-Z-]*.readthedocs.io/?|https?://docs.rs/[a-z0-9_A-Z-]*/?|https?://.*(documentation).*/)[^ >\)]*' | grep -v -E ".(svg|png|jpg)$" | sort | uniq || exit 1
+grep -o -E '(https?://[a-z0-9_A-Z-]*.readthedocs.io/?|https?://docs.rs/[a-z0-9_A-Z-]*/?|https?://[^ >)]*(documentation)[^ >)]*/?)[^ >)]*' | grep -v -E ".(svg|png|jpg|gif)$" | sort | uniq || exit 1
 
