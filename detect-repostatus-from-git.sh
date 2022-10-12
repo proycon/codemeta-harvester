90fac5fffaeef6233b40e7f6f753db62cb038073
Maarten van Gompel <proycon@anaproy.nl>
2022-10-12 12:55:38 +0200

    detect-repostatus-from-git: allow passing explicit version (will ignore git tags then)
    This leads to more accurate repostatus for repositories that, although
    required, don't use git tags

--- detect-repostatus-from-git.sh
+++ detect-repostatus-from-git.sh
@@ -5,13 +5,14 @@
 # and at what time the release or the latest git update to master/main was.
 # if it's longer than three quarters of a year, we choose an inactive or suspended status
 
+VERSION="$1" #explicitly passed version, will ignore git tags if set
 NOW=$(date +%s)
 TIMESTAMP_UPDATED=$(git log -1 --date=format:"%s" --format="%ad")
 TIMESTAMP_LATEST_TAG=$(git log --date=format:"%s" --format="%ad %D" | grep -E "tag: v?[0-9]" | head -n 1 | cut -d " " -f 1)
 THREEQYEARAGO=$((NOW - 23587200))  #three quarters of a year ago (273 days in seconds)
-if [ -n "$TIMESTAMP_LATEST_TAG" ]; then
+if [ -n "$TIMESTAMP_LATEST_TAG" ] || [ -n "$VERSION" ]; then
     #there have been releases
-    if [ $TIMESTAMP_UPDATED -gt $THREEQYEARAGO ] || [ $TIMESTAMP_LATEST_TAG -gt $THREEQYEARAGO ]; then
+    if [ "$TIMESTAMP_UPDATED" -gt $THREEQYEARAGO ] || [ "$TIMESTAMP_LATEST_TAG" -gt $THREEQYEARAGO ]; then
         echo "https://www.repostatus.org/#active"
     else
         echo "https://www.repostatus.org/#inactive"
@@ -19,7 +20,7 @@ if [ -n "$TIMESTAMP_LATEST_TAG" ]; then
     fi
 else
     #there have been no releases
-    if [ $TIMESTAMP_UPDATED -gt $THREEQYEARAGO ]; then
+    if [ "$TIMESTAMP_UPDATED" -gt $THREEQYEARAGO ]; then
         echo "https://www.repostatus.org/#wip"
     else
         echo "https://www.repostatus.org/#suspended"
