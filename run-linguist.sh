e05d7f0dad58fd4d48e4b9167d611af8e5c1c9b4
Jens Bröder <j.broeder@fz-juelich.de>
2022-10-19 12:05:41 +0200

    Fix path bug for run-linguist

--- run-linguist.sh
+++ run-linguist.sh
@@ -1,8 +1,10 @@
+#!/bin/sh
 
+SCRIPTDIR="$(realpath "$(dirname "$0")")"
 
 # only works if executed in the directory of the source code repo
-github-linguist -b -j > res.json
+github-linguist -b -j > tmp_codemetea_linguist_res.json
 
-python parse_liguist_json.py res.json
+python $SCRIPTDIR/parse_liguist_json.py tmp_codemetea_linguist_res.json
 
-rm res.json
+rm tmp_codemetea_linguist_res.json
